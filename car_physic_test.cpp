97b49329a675b7648488d9068b17e5ced94f450a
Lyapunov <vaskorichard@gmail.com>
2016-05-11 06:07:39 +0200

    Adding more debug info, visualizing turning radius.

--- car_physic_test.cpp
+++ car_physic_test.cpp
@@ -23,6 +23,31 @@
 #include <GL/glu.h>                // GLU
 #include <GL/gl.h>                 // OpenGL
 
+// From:
+// http://slabode.exofire.net/circle_draw.shtml
+void DrawCircle(float cx, float cy, float r, int num_segments) 
+{ 
+   float theta = 2 * 3.1415926 / float(num_segments); 
+   float c = cosf(theta);//precalculate the sine and cosine
+   float s = sinf(theta);
+   float t;
+
+   float x = r;//we start at angle = 0 
+   float y = 0; 
+       
+   glBegin(GL_LINE_LOOP); 
+   for(int ii = 0; ii < num_segments; ii++) 
+   { 
+      glVertex2f(x + cx, y + cy);//output vertex 
+      //apply the rotation matrix
+      t = x;
+      x = c * x - s * y;
+      y = s * t + c * y;
+   } 
+   glEnd(); 
+}
+   
+
 //-----------------------------------------------------------------------
 // Global data
 //-----------------------------------------------------------------------
@@ -225,6 +250,14 @@ public:
       glTranslatef(x_, y_, 0.0f);
       glRotatef(angleOfCarOrientation_, 0.0, 0.0, 1.0);
       glTranslatef(0, params_.getDistanceBetweenCenterAndTurningAxle(), 0.0f);
+
+      // debug info
+      glColor3fv(YELLOW_RGB);
+      if ( fabs( wheelOrientation_ ) > 1. ) {
+         DrawCircle( -sign( wheelOrientation_ ) * turningBaselineDistance_, -params_.getCarHeight() / 2. + params_.getCarHeightLower(), turningRadius_, 200 );
+      }
+
+      // car visualization
       glColor3fv(BLUE_RGB);
       const float w2 = params_.getCarWidth() / 2.;
       const float h2 = params_.getCarHeight() / 2.;
